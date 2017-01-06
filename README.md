8bc64af005a346ffdb694ca6fe5c1959429bd5bf
AliceOrwell <AliceOrwell@users.noreply.github.com>
2017-01-06 23:44:29 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # TSW-Roll-For-Role
 Webtool to randomly assign roles for The Secret World
+
+A live version of the project can be found at: https://aliceorwell.github.io/TSW-Roll-For-Role/
+
+Structure
+------------
+`index.html` - The web interface.
+
+`/src/` - Javascript and CSS assets for the library.
+
+`/js/` - Third Party Javascript assets.
+
+`/css/` - CSS assets.
+
+Contributing
+------------
+Pull requests are welcome!
+
+Attribution
+-------
+DOM manipulation is done using [jQuery](https://jquery.com/).
+
+License
+-------
+Copyright (c) 2016 AliceOrwell
+
+Software released under the MIT License. Please see the [LICENSE](LICENSE) file.
---
+++ src/tswrollforrole.js
@@ -0,0 +1,95 @@
+/**********************************************************
+	TSW Roll For Role
+	https://github.com/AliceOrwell/TSW-Roll-For-Role/
+
+	Released under the MIT license
+ **********************************************************/
+
+var rolePresets = {
+  "Custom": [],
+  "Eidolon Elite": [
+    "Main Tank", "Off Tank", "Healer", "DABS", "DABS", "DPS", "DPS", "DPS", "DPS"
+  ],
+  "Flappy NM": [
+    "Tank (Red)", "Tank (Purple)", "Healer", "DABS (Red)", "DABS (Purple)",
+    "Shade 1", "Shade 2", "Shade 2", "Backup"
+  ],
+  "NYR Elite": [
+    "DPS Tank", "Healtank", "DABS", "DABS", "SF", "SF", "DPS", "DPS", "DPS"
+  ]
+};
+
+
+var assignment = function( subject, object ) {
+  this.subject = "UNASSIGNED";
+  this.object = "UNASSIGNED";
+  this.isAssigned = false;
+
+  if ( object ) {
+    this.object = object;
+    this.isAssigned = true;
+  }
+  if ( subject ) {
+    this.subject = subject;
+    this.isAssigned = true;
+  }
+
+  this.toString = function() {
+    var output = this.subject + " := " + this.object;
+    return output;
+  }
+
+  return this;
+};
+
+var tswRollForRole = {
+  roleExtracter: function( roles ) {
+    if ( roles == "" ) {
+      return [];
+    }
+    return roles.split( '\n' );
+  },
+  assigner: function ( roles, people ) {
+    var assignments = [];
+
+    var subjects = roles;
+    var objects = people;
+    if ( roles.length < people.length ) {
+      subjects = people;
+      objects = roles;
+    }
+
+    for( var i = 0; i < subjects.length; i++ ) {
+      var subject = subjects[i];
+      var object = false;
+      if ( i < objects.length ) {  // If there are objects left to assign
+        object = objects[i];
+      }
+
+      var ass = new assignment( subject, object );
+      assignments.push( ass );
+    }
+    return assignments;
+  },
+  roleData: function( preset ) {
+    if ( rolePresets.hasOwnProperty(preset) ) {
+      var p = rolePresets[preset];
+      p = p.toString()
+      p = p.split(',').join('\n');  // Replace all commas for newlines
+      return p;
+    }
+    return "";
+  },
+  format: function( assignments ) {
+    var output = assignments.length + " Assignment(s)";
+    if ( assignments.length == 0 ) {
+      return output;
+    }
+
+    output += " -- " + assignments[0].toString();
+    for(var i = 1; i < assignments.length; i++) {
+      output +=  " :: " + assignments[i].toString();
+    }
+    return output;
+  }
+};
